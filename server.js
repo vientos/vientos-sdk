ce113766fd1b21d92080f61f32a860e5b708e7db
elf Pavlik <elf-pavlik@hackers4peace.net>
2017-06-06 11:53:08 -0500

    dev login with specific person id

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
 const cuid = require('cuid')
 const server = require('./vientos-service')
-const person = require('vientos-fixtures').people[0]
+const Person = require('./vientos-service/src/models/person')
 const PWA_URL = process.env.PWA_URL || 'http://localhost:8080'
 
-server.route({
+const handler = function (request, reply) {
+  let query = Person.findOne({})
+  if (request.params.id) {
+    let iri = process.env.OAUTH_CLIENT_DOMAIN + '/people/' + request.params.id 
+    query = Person.findOne({ _id: iri })
+  }
+  query.then(person => {
+    request.cookieAuth.set({
+      id: person._id,
+      sessionId: cuid()
+    })
+    reply().redirect(PWA_URL)
+  })
+}
+
+server.route([{
   method: 'GET',
   path: '/auth/dev',
   config: {
     auth: false,
-    // TODO import handler from vientos-service
-    handler: (request, reply) => {
-      request.cookieAuth.set({
-        id: process.env.OAUTH_CLIENT_DOMAIN + '/people/' + person._id,
-        sessionId: cuid() })
-      reply().redirect(PWA_URL)
-    }
+    handler
   }
-})
+}, {
+  method: 'GET',
+  path: '/auth/dev/{id}',
+  config: {
+    auth: false,
+    handler
+  }
+}])
 
 server.start((err) => {
   if (err) throw err
